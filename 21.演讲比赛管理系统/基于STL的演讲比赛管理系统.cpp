3d583853515028b9fe9e039bae341b3471c227de
史金伟 <1627961573@qq.com>
2021-05-05 21:03:02 +0800

    21.演讲比赛管理系统

--- 21.演讲比赛管理系统/基于STL的演讲比赛管理系统.cpp
+++ 21.演讲比赛管理系统/基于STL的演讲比赛管理系统.cpp
@@ -1,5 +1,31 @@
 #include"speechManger.h"
 int main()
 {
+	//创建一个管理类的对对象
+	SpeechManger sm;
+	int choice = 0;
+	while (true)
+	{
+		sm.show_menu();
+		cout << "请输入您的选择：" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1://开始比赛
+			break;
+		case 2://查看往届记录
+			break;
+		case 3://清空比赛记录
+			break;
+		case 0://退出系统
+			sm.exitsystem();
+			break;
+		default:
+			system("cls");
+			break;
+		}
+	}
 
+	system("pause");
+	return 0;
 }
