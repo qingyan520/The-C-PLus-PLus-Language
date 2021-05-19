8fe7dfc1f3904ce9f297dae6be73ed9c108201de
qingyan520 <1627961573@qq.com>
2021-05-19 21:01:14 +0800

    test_5_18_C++入门

--- 2.C++初阶/test_5_18_C++入门/test.cpp
+++ 2.C++初阶/test_5_18_C++入门/test.cpp
@@ -86,12 +86,97 @@
 //}
 
 
+//
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	void* p = new char[0xfffffffful];
+//	//cout << "new:" << p << endl;
+//	return 0;
+//}
 
-#include <iostream>
-using namespace std;
-int main()
-{
-	void* p = new char[0xfffffffful];
-	//cout << "new:" << p << endl;
-	return 0;
-}
+
+//函数重载：即在C++中容许有同名的函数
+//函数重载的条件：函数的参数类型不同，返回值不同
+//注：函数的返回值不能构成函数重载的条件
+//例：
+//#include<iostream>
+//using namespace std;
+//void Print(int a)
+//{
+//	cout << a << endl;
+//}
+//void Print(double a)
+//{
+//	cout << a << endl;
+//}
+//int main()
+//{
+//	Print(1.0);
+//	Print(1);
+//	return 0;
+//}
+//如上述例子所述，我们要实现两个自定义打印函数，一个打印整数，一个打印浮点数，两个函数列表的参数类型不同
+//而函数名却相同，但是我们在编译时却不会报错，这就是函数重载，让我们实现了打印两个不同类型打印函数
+//而在C语言中我们就要起两个函数名来实现这件事，如Print_int,Print_double
+//那么为什么会出现函数重载这件事呢，这就要从编译链接的过程开始说起：
+//1.预处理：头文件的展开+宏替换+去掉注释+条件编译
+//2.编译：检查语法，将代码转化成汇编代码
+//3.汇编：汇编代码转化为二进制的机器码
+//4.链接：将所有文件连接在一起，生成可执行程序
+//而C++在编译过程中有函数名修饰规则:函数的返回值+函数名+函数参数类型，这样每个函数就能够进行独立识别，既不会产生命名冲突
+
+
+
+//引用：
+//引用不是重新写一个变量，而是给一个变量起别名，他和他引用的变量共同访问一块内存空间
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a = 10;
+//	int& b = a;
+//	cout << a << endl;
+//	cout << b << endl;
+//	b = 100;
+//	cout << a << endl;
+//	cout << b << endl;
+//	system("pause");
+//	return 0;
+//}
+
+//引用可以做函数返回值，引用当形参列表
+//
+//
+//#include<iostream>
+//void  Print(int a)
+//{
+//	cout << a << endl;
+//}
+//void Print(string str)
+//{
+//	cout << str << endl;
+//}
+//using namespace std;
+//int main()
+//{
+//	Print(1);
+//	Print("hello  world");
+//}
+
+//#include<iostream>
+//using namespace std;
+//int Ad(int a, int b)
+//{
+//	return a + b;
+//}
+//void func(int a,  double b, int* p)
+//{
+//
+//}
+//int main()
+//{
+//
+//	return 0;
+//}
