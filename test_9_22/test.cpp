94666485d3dedd0798dc1ed6cfca63d4f5d37191
qingyan520 <1627961573@qq.com>
2021-10-30 21:37:55 +0800

    test_9_22

---
+++ test_9_22/test.cpp
@@ -0,0 +1,64 @@
+﻿#include<iostream>
+using namespace std;
+void encrypt(char info[]);
+int main()
+{
+    // ÇëÔÚ´ËÌí¼ÓÄãµÄ´úÂë
+   /********** Begin ********/
+    char str[7] = "\0";
+    gets_s(str);
+   /********** End **********/
+    encrypt(str);
+   // cout << str << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << str[i];
+    }
+    //cout << end;
+}
+void encrypt(char info[])
+{
+    // ÇëÔÚ´ËÌí¼ÓÄãµÄ´úÂë
+   /********** Begin ********/
+    for (int i = 0; i < 6; i++)
+    {
+        if (info[i] == 'w')
+        {
+            info[i] = 'a';
+        }
+        else if (info[i] == 'x')
+        {
+            info[i] = 'b';
+        }
+        else if (info[i] == 'y')
+        {
+            info[i] = 'c';
+        }
+        else if (info[i] == 'z')
+        {
+            info[i] = 'd';
+        }
+        else if (info[i] == 'W')
+        {
+            info[i] = 'A';
+        }
+        else if (info[i] == 'X')
+        {
+            info[i] = 'B';
+        }
+        else if (info[i] == 'Y')
+        {
+            info[i] = 'C';
+        }
+        else if (info[i] == 'Z')
+        {
+            info[i] = 'D';
+        }
+        else if(info[i]!='\0')
+        {
+            info[i] += 4;
+        }
+    }
+   /********** End **********/
+}
+
