628d2f2c98460e6f71213be48e5bd390d8446032
qingyan520 <1627961573@qq.com>
2021-10-30 21:45:13 +0800

    test_9_27

---
+++ test_9_27/源.cpp
@@ -0,0 +1,26 @@
+/********* Begin *********/
+#include <iostream>
+using namespace std;
+
+/********* Begin *********/
+//在此处实现一个汽车类
+class Car
+{
+
+
+
+};
+/********* End *********/
+
+int main()
+{
+    /********* Begin *********/
+    //在此处解析执行输出汽车的整体状态
+    char cmds[25];
+    cin >> cmds;
+
+
+
+
+    /********* End *********/
+}
