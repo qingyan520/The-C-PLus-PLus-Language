737f9966aabebd71dde0b3213b497f579ae756c7
qingyan520 <1627961573@qq.com>
2021-10-30 21:33:27 +0800

    test_9_21

---
+++ test_9_21/test.cpp
@@ -0,0 +1,21 @@
+//C++�еļ̳�Ԥϰ
+#include<iostream>
+using namespace std;
+class Person
+{
+public:
+public:
+	string name;
+	int age;
+};
+class Student:Person
+{
+public:
+
+};
+int main()
+{
+	Student S;
+	
+	return 0;
+}
