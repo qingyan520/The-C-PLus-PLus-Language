cdd2a9491a2b12a2f6b3ebfb41e6c52b65d68c35
qingyan520 <1627961573@qq.com>
2021-11-05 21:47:22 +0800

    test_11_5

---
+++ test_11_5/test.cpp
@@ -0,0 +1,39 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int x(int& m, int& n)
+//{
+//    int ret = 0;
+//    if (m < n)
+//    {
+//        swap(m, n);
+//    }
+//    for (int i = 1; i <= n; i++)
+//    {
+//        if (m % i == 0 && n % i == 0)
+//        {
+//            if (i > ret)
+//            {
+//
+//                i = ret;
+//            }
+//        }
+//    }
+//
+//    return ret;
+//
+//}
+//int main()
+//{
+//    int n, a;
+//    while (1)
+//    {
+//        cin >> n;
+//        cin >> a;
+//        int c = x(n, a);
+//        cout << c << endl;
+//    }
+//}
+
+
+#include<>
