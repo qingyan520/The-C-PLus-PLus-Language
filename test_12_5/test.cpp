aa883da49c3f5174f85858fd836112702c185b5a
qingyan520 <1627961573@qq.com>
2021-12-06 21:56:06 +0800

    test_12_5

---
+++ test_12_5/test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class A
+{
+public:
+	A& operator+(const A& m1,const m2);
+private:
+	int _a;
+};
+A& A::operator+(const A& m1,const m2)
+{
+
+}
+int main()
+{
+
+	return 0;
+}
